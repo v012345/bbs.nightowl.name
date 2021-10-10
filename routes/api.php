86870c4599cfbe395e10548c9d556e1eb22f41fe
Meteor <v138468@gmail.com>
2021-10-10 18:39:53 +0800

    application/javascript

--- routes/api.php
+++ routes/api.php
@@ -24,5 +24,5 @@ Route::get('vue/{delay}', function ($delay) {
     sleep(intval($delay));
     $client = new Client;
     $res = $client->request('GET', 'https://cdn.jsdelivr.net/npm/vue@2.6.14/dist/vue.js');
-    return $res->getBody();
+    return response($res->getBody(), 200)->header('Content-Type', "application/javascript");
 });
